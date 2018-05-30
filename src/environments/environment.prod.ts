6e0d7928e1b72772fdfcf71523db677df91492fd
jkaller <32413228+jkaller@users.noreply.github.com>
2018-05-30 22:28:06 +0900

    Add files via upload

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+  apiKey: "AIzaSyCl3rMf8KfEiFSCx5_h2JrYjg9LZTKZu70",
+    authDomain: "angularfiretest-31f86.firebaseapp.com",
+    databaseURL: "https://angularfiretest-31f86.firebaseio.com",
+    projectId: "angularfiretest-31f86",
+    storageBucket: "angularfiretest-31f86.appspot.com",
+    messagingSenderId: "138050643282"
+  }
+};
